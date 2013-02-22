e0e488e41b78be3394216bc037ccfc11eedee718
ITRO <ITRO@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-22 20:27:32 +0000

    upload version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/itro-popup/tags/1.0.2@671892 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions/admin-function.php
@@ -0,0 +1,254 @@
+<?php
+/*
+Copyright 2013  I.T.RO.® Corp  (email : support.itro@live.com)
+This file is part of ITRO Popup Plugin.
+
+    ITRO Popup Plugin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ITRO Popup Plugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ITRO Popup Plugin.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Administration menu
+function itro_plugin_menu() {
+	add_options_page( 'Popup Plugin Options', 'ITRO Popup', 'manage_options', 'itro-plugin-menu', 'itro_plugin_options' );
+}
+
+function itro_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	// variables for the field and option names
+	if( !isset($submitted_form )) 
+	{
+		$opt_name=array(
+		/*opt 0*/'popup_time',
+		/*opt 1*/'only_home',
+		/*opt 2*/'cookie_time_exp',
+		/*opt 3*/'popup_width',
+		/*opt 4*/'popup_background',
+		/*opt 5*/'popup_border_color',
+		/*opt 6*/'age_restriction',
+		/*opt 7*/'enter_button_text',
+		/*opt 8*/'leave_button_text',
+		/*opt 9*/'leave_button_url',
+		/*opt 10*/'enter_button_bg_color',
+		/*opt 11*/'enter_button_border_color',
+		/*opt 12*/'leave_button_bg_color',
+		/*opt 13*/'leave_button_border_color',
+		/*opt 14*/'enter_button_font_color',
+		/*opt 15*/'leave_button_font_color',
+		);
+		$field_name=array(
+		/*fld 1*/'custom_html',
+		);
+		$submitted_form = 'mt_submit_hidden';
+	}
+	
+	for($i=0;$i<count($opt_name); $i++)
+	{
+		// Read in existing option value from database
+		$opt_val[$i] = itro_get_option( $opt_name[$i] );
+
+		// See if the user has posted us some information
+		// If they did, this hidden field will be set to 'Y'
+		if( isset($_POST[ $submitted_form ]) && $_POST[ $submitted_form ] == 'Y')
+		{
+			// Read their posted value
+			if(isset($_POST[$opt_name[$i]])){$opt_val[$i] = $_POST[ $opt_name[$i] ];}
+			else{$opt_val[$i] = NULL;}
+			
+			// Save the posted value in the database
+			itro_update_option( $opt_name[$i], $opt_val[$i] );
+		}
+	}
+	for($i=0;$i<count($field_name); $i++)
+	{
+		// Read in existing option value from database
+		$field_value[$i] = itro_get_field( $field_name[$i] );
+
+		// See if the user has posted us some information
+		// If they did, this hidden field will be set to 'Y'
+		if( isset($_POST[ $submitted_form ]) && $_POST[ $submitted_form ] == 'Y' ) 
+		{
+			// Read their posted value
+			if(isset($_POST[$field_name[$i]])){$field_value[$i] = $_POST[ $field_name[$i] ];}
+			else{$field_value[$i] = NULL;}
+			
+			// Save the posted value in the database
+			itro_update_field( $field_name[$i], $field_value[$i] );
+		}
+	}
+	
+	// Put an settings updated message on the screen
+	if( isset($_POST[ $submitted_form ]) && $_POST[ $submitted_form ] == 'Y' ) {
+		?>
+		<div class="updated"><p><strong><?php _e('settings saved.', 'itro-plugin' ); ?></strong></p></div>
+		<?php
+	}
+	?>
+	<img style="position:absolute" src="http://www.gnu.org/graphics/gplv3-88x31.png">
+<h1 style="text-align:center"><?php _e( 'I.T.RO. Popup Plugin', 'itro-plugin') ?></h1>
+<h2 style="text-align:center"><?php _e( 'Settings', 'itro-plugin') ?></h2>
+<div id="rightColumn">
+
+	<!-- Donation form - please don't change or remove!!! thanks !-->
+	<div id="donateForm">
+		<h3><?php _e("HELP US TO CONTINUE OUR DEVELOPING WORK. PLEASE DONATE!")?></h3>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="L2QKQKSPMY3RU">
+			<table style="float:right;">
+				<tr><td><input type="hidden" name="on0" value="Make your donation">Make your donation</td></tr><tr><td><select name="os0">
+				<option value="thiny donation">thiny donation &#8364;1,00 EUR</option>
+				<option value="little donation">little donation &#8364;2,00 EUR</option>
+				<option value="right donation">right donation &#8364;5,00 EUR</option>
+				<option value="normal donation">normal donation &#8364;10,00 EUR</option>
+				<option value="good donation">good donation &#8364;20,00 EUR</option>
+				<option value="great donation">great donation &#8364;50,00 EUR</option>
+				</select> </td></tr>
+			</table>
+			<input type="hidden" name="currency_code" value="EUR">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+		</form>
+	</div>
+	
+	<!-- Color Table !-->
+	<object>
+		<param name="movie" value="file.swf">
+		<embed id="colorTable" src="<?php echo itroPath . 'other/color.swf'?>" width="100" height="100">
+		</embed>
+	</object>
+	<?php
+		//---Image manager form for the popup image
+		
+		echo itro_onOff('imgManagerForm');//the hide-show function?>
+		<p class="wpstyle" onClick="onOff_imgManagerForm();"><?php _e("Popup image settings:", 'itro-plugin' ); ?> </p>
+		<?php 
+		if(!empty($_REQUEST['submitUpload'])) {itro_image_uploader(); itro_update_option('img_url_check',NULL);}//if use has uploaded an image
+		if(!empty($_REQUEST['submitDelete']) || !empty($_REQUEST['submitSelect'])) {itro_image_manager(); itro_update_option('img_url_check',NULL);}//if use has managed images
+		if( isset($_POST[ 'img_url_check' ]) && $_POST[ 'image_manager' ] == 'Y' ) //if user has selcted the direct url
+		{
+			itro_update_option('img_url_check',$_POST['img_url_check']);
+			itro_update_option('img_source',$_POST['img_source']);
+			itro_update_option('img_direct_url',$_POST['img_source']);
+		}
+		?>
+		<form id="imgManagerForm" action="#imgManagerForm" method="post" enctype="multipart/form-data">
+			<input type="hidden" name="image_manager" value="Y">
+			<?php echo itro_onOff_checkbox('imgUrlCheck','imgUploader','true');?>
+			<p><?php _e("Use direck url:", 'itro-plugin' ); ?>
+				<input id="imgUrlCheck" type="checkbox" onClick="imgUrlCheck_checkbox_imgUploader()" name="img_url_check" value="yes" <?php if(itro_get_option('img_url_check')=='yes' ){echo 'checked="checked"';} ?> />
+				<input type="text" name="<?php echo 'img_source'; ?>" value="<?php echo itro_get_option('img_direct_url'); ?>" size="50">
+				<input class="button-primary" type="submit" name="submitImgUrl" value="<?php _e("Save", 'itro-plugin')?>">
+			</p>
+			<div id="imgUploader" style="height:<?php if(itro_get_option('img_url_check')=='yes'){echo '0px;';}?>">
+				<p><?php _e('Current image link:','itro-plugin')?><a href="<?php echo itro_get_option('img_source');?>"><?php echo itro_get_option('img_source');?></a></p>
+				<select name="selected_image" style="min-width:100px;">
+					<?php itro_image_list(); ?>
+				</select>
+				<input class="button-primary" type="submit" name="submitSelect" value="<?php _e("Select", 'itro-plugin')?>">
+				<input class="button-primary" type="submit" name="submitDelete" value="<?php _e("Delete", 'itro-plugin')?>">
+				<p>
+					<label for="file"><?php _e("Select the image", 'itro-plugin')?></label>
+					<input type="file" name="file" id="file"><br>
+					<input class="button-primary" type="submit" name="submitUpload" value="<?php _e("Upload", 'itro-plugin')?>">
+					<input type="checkbox" name="overwrite" value="yes"><?php _e("Overwrite", 'itro-plugin')?>
+				</p>
+			</div>
+		</form>
+</div>
+
+<div id="leftColumn">
+
+	<!-- Settings form !-->
+	<div id="formContainer">
+		<form id="settingsForm" method="post" action="#settingsForm">
+		
+			<!--------- General options --------->
+			<?php echo itro_onOff('genOption');?>
+			<p class="wpstyle" onClick="onOff_genOption();"><?php _e("General Popup Option:", 'itro-plugin' ); ?> </p>
+			<div id="genOption">
+				<input type="hidden" name="<?php echo $submitted_form; ?>" value="Y">
+				<p><?php _e("Popup seconds:", 'itro-plugin' ); ?> 
+					<input type="text" name="<?php echo $opt_name[0]; ?>" value="<?php echo $opt_val[0]; ?>" size="10">
+				</p>
+				<p><?php _e("Cookie Time Expiration (hours):", 'itro-plugin' ); ?> 
+					<input type="text" name="<?php echo $opt_name[2]; ?>" value="<?php echo $opt_val[2]; ?>" size="10">
+				</p>
+				<p><?php _e("Popup width (%):", 'itro-plugin' ); ?> 
+					<input type="text" name="<?php echo $opt_name[3]; ?>" value="<?php echo $opt_val[3]; ?>" size="10">
+				</p>
+				<p><?php _e("Popup background color", 'itro-plugin' ); ?> 
+					<input type="text" name="<?php echo $opt_name[4]; ?>" value="<?php echo $opt_val[4]; ?>" size="10">
+				</p>
+				<p><?php _e("Popup border color:", 'itro-plugin' ); ?> 
+					<input type="text" name="<?php echo $opt_name[5]; ?>" value="<?php echo $opt_val[5]; ?>" size="10">
+				</p>
+				<p><?php _e("Only in home page:", 'itro-plugin' ); ?> 
+					<input type="checkbox" name="<?php echo $opt_name[1]; ?>" value="yes" <?php if($opt_val[1]=='yes' ){echo 'checked="checked"';} ?> />
+				</p>
+			</div>
+			
+			<!------- Custom html field -------->
+			<?php echo itro_onOff('customHtmlForm');?>
+			<p class="wpstyle" onClick="onOff_customHtmlForm();"><?php _e("Custom HTML code:", 'itro-plugin' ); ?> </p>
+			<p id="customHtmlForm">
+				<textarea rows="9" cols="70" name="<?php echo $field_name[0]; ?>"><?php echo $field_value[0]; ?></textarea>
+			</p>
+
+			<!------------ Age restriction option  ---------->
+			<?php echo itro_onOff('ageRestSettings');?>
+			<p class="wpstyle" onClick="onOff_ageRestSettings();"><?php _e("Age restriction settings:", 'itro-plugin' ); ?> </p>
+			<div id="ageRestSettings">
+				<?php echo itro_onOff_checkbox('ageCheck','ageRest','false');?>
+				<p><?php _e("Age restricted page:", 'itro-plugin' ); ?>
+				<input id="ageCheck" type="checkbox" onClick="ageCheck_checkbox_ageRest()" name="<?php echo $opt_name[6]; ?>" value="yes" <?php if($opt_val[6]=='yes' ){echo 'checked="checked"';} ?> />
+				</p>
+				<div id="ageRest" style="height:<?php if($opt_val[6]!='yes' ){echo '0px;';}?>">
+					<p><?php _e("Enter button text:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[7]; ?>" value="<?php if($opt_val[7]==NULL){_e("i.e.: I AM OVER 18 - ENTER", 'itro-plugin' );} else{echo $opt_val[7];} ?>" size="40">
+					</p>
+					<p><?php _e("Enter button background color:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[10]; ?>" value="<?php echo $opt_val[10]; ?>" size="10">
+					</p>
+					<p><?php _e("Enter button border color:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[11]; ?>" value="<?php echo $opt_val[11]; ?>" size="10">
+					</p>
+					<p><?php _e("Enter button font color:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[14]; ?>" value="<?php echo $opt_val[14]; ?>" size="10">
+					</p>
+					<p><?php _e("Leave button text:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[8]; ?>" value="<?php if(!isset($opt_val[8])){_e("i.e.: I AM UNDER 18 - LEAVE", 'itro-plugin' );} else{echo $opt_val[8];} ?>" size="40">
+					</p>
+					<p><?php _e("Leave button url:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[9]; ?>" value="<?php if(!isset($opt_val[9])){_e("i.e.: http://www.mysite.com/leave.html", 'itro-plugin' );} else{echo $opt_val[9];} ?>" size="40">
+					</p>
+					<p><?php _e("Leave button background color:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[12]; ?>" value="<?php echo $opt_val[12]; ?>" size="10">
+					</p>
+					<p><?php _e("Leave button border color:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[13]; ?>" value="<?php echo $opt_val[13]; ?>" size="10">
+					</p>
+					<p><?php _e("Leave button font color:", 'itro-plugin' ); ?> 
+						<input type="text" name="<?php echo $opt_name[15]; ?>" value="<?php echo $opt_val[15]; ?>" size="10">
+					</p>					
+				</div>
+			</div>
+			
+			<p class="submit">
+			<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+			</p>
+		</form>
+	</div>	
+</div>
+<?php } ?>
---
+++ readme.txt
@@ -0,0 +1,87 @@
+=== ITRO Popup Plugin ===
+Contributors: I.T.RO. &#174; Corp
+Donate link: http://www.itro.eu/index.php/donate/
+Tags: popup, age, restriction, block, violence
+Requires at least: 3.0.1
+Tested up to: 3.5.1
+Stable tag: 1.0.2
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+ITRO Popup Plugin enable user to add a full-optional popup to their Wordpress site, also with legal block for adult-content site and
+custom HTML code.
+ITRO Popup Plugin permette di aggiungere un popup totalmente personalizzabile, con la possibilità di inserire il blocco legale per 
+siti con contentuti non adatti a fascie di età protette (violenza, vendita tabacco o alcool, linguaggio forte, ecc...). Possibilità di 
+inserire codice HTML personalizzato.
+
+== Description ==
+
+ITRO Popup is a very lightweight (less than 200Kb!) Plugin which allow users to add a fully-customizable  and perfectly centered popup. 
+It has a very user-friendly interface, which allow inexpert user to easily modify all  settings needed for a popup. 
+With "Age restricted site"  option, the popup block the navigation until the user declare that is above the local country legal age. 
+It is useful for adult content site (tobacco & alcohol-selling, violence, bad language, etc...). The leave and the enter button 
+are separately customizable. Advanced user can also add their own HTML code. 
+
+MAKE YOUR DONATION - HELP US TO DEVELOP FOR YOU! [DONATE NOW](http://www.itro.eu/index.php/donate/)
+
+== Installation ==
+
+1. Unzip `itro-plugin.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to "Settings->ITRO Popup" in the admin menu
+4. Enjoy
+
+== Frequently Asked Questions ==
+= How can i quickly test my settings and delete cookie so the page display the popup? =
+
+You need to set the "Cookie expiration time" to zero and refresh your website page a couple of time.
+
+= Does the plugin add some tables in my Wordpress database? =
+
+Yes. The tables are "wp_itro_plugin_option" "wp_itro_plugin_field".
+
+= What are they for? =
+
+In the first one are stored all small data option, in the other one bigger data as custom HTML code.
+
+== Screenshots ==
+
+
+
+== Changelog ==
+= 1.0.2 =
+bugs fixed;
+now popup is better centered;
+added the possibility to insert a direct url for the popup image.
+
+= 1.0.1 =
+bug fixed: color table now works in admin pannel.
+
+= 1.0 =
+First released version.
+
+== Upgrade Notice ==
+= 1.0.2 =
+bugs fixed;
+added the possibility to insert a direct url for the popup image;
+100% of compability;
+
+= 1.0.1 =
+need to upgrade, now the color table works right.
+
+== Notes ==
+
+This is a totaly free software we have spent a lot of time to develop it. In the future we'll update 
+and add new features and we accept all kind of (smart) suggestion. So please donate something 
+to help us continue our work.
+
+== Features ==
+
+* Fully translated in Italian. Ready for multilingual translation.
+* Perfectly centered popup, also with resized windows
+* Fully and easy customizable aspect (background, border, color, etc...)
+* Settable countdown for automatic closing
+* Settable coockie expiration time for the next popup visualization
+* Possibility to block for adult-content site
+* Custom HTML code to display whatever you want
+* All in a very easy way to use!
